10948e831a5eab882061f4de6ff56a28d69eb733
ramanji13 <venkataramanjireddy13@gmail.com>
2024-11-24 13:08:26 +0530

    Different patterns

--- patt.c
+++ patt.c
@@ -96,3 +96,18 @@ int main(){
     }printf("\n");
 }
 }
+#include<stdio.h>
+
+int main()
+{   int i,j,n,number=1;
+    printf("enter value for number of rows");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++) {
+        for(j=1; j<=i; j++) {
+            printf("%d",number);
+            number++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
